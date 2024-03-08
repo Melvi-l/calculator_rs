d976a49120e8d5f85f504ffb474985e11da27a98
Melvi-l <melvil.miglio@gmail.com>
2024-03-08 23:18:02 +0100

    add a repl !

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+use std::io::{self, Write};
+
+use calc_lib::calc;
+
 pub fn main() {
-    println!("Hello world")
+    println!("Hello world");
+    let mut input = String::new();
+
+    loop {
+        print!("calc >> ");
+        io::stdout().flush().unwrap();
+
+        if io::stdin().read_line(&mut input).unwrap() == 0 {
+            break;
+        }
+
+        let resultat = calc(input.trim());
+        println!("Result: {}", resultat);
+
+        input.clear();
+    }
 }
 
 
